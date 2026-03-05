1eb9a0774f7bf6d89d6d30bbd2eeb3c72b980f49
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-03-05 14:21:16 -0500

    Run HAMR Codegen on model

---
+++ Models/Status/Status_v2/Hardened/hamr/microkit/components/cpuSw_logMonitor_logMonitor/src/cpuSw_logMonitor_logMonitor_user.c
@@ -0,0 +1,19 @@
+#include "cpuSw_logMonitor_logMonitor.h"
+
+// This file will not be overwritten if codegen is rerun
+
+void cpuSw_logMonitor_logMonitor_initialize(void) {
+  printf("%s: cpuSw_logMonitor_logMonitor_initialize invoked\n", microkit_name);
+}
+
+void cpuSw_logMonitor_logMonitor_timeTriggered(void) {
+  printf("%s: cpuSw_logMonitor_logMonitor_timeTriggered invoked\n", microkit_name);
+}
+
+void cpuSw_logMonitor_logMonitor_notify(microkit_channel channel) {
+  // this method is called when the monitor does not handle the passed in channel
+  switch (channel) {
+    default:
+      printf("%s: Unexpected channel %d\n", microkit_name, channel);
+  }
+}
