a7b7b19e49064903d50e27caf05e0a77ba9622b0
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-03-10 13:35:52 -0400

    Rerun HAMR codegen from scratch

--- Models/Status/Status_v2/Hardened/hamr/microkit/crates/cpuSw_logMonitor_logMonitor/src/test/tests.rs
+++ Models/Status/Status_v2/Hardened/hamr/microkit/crates/cpuSw_logMonitor_logMonitor/src/test/tests.rs
@@ -21,3 +21,61 @@ mod tests {
     crate::cpuSw_logMonitor_logMonitor_timeTriggered();
   }
 }
+
+mod GUMBOX_tests {
+  use serial_test::serial;
+  use proptest::prelude::*;
+
+  use crate::test::util::*;
+  use crate::testInitializeCB_macro;
+  use crate::testComputeCB_macro;
+    use crate::testComputeCBwGSV_macro;
+
+  // number of valid (i.e., non-rejected) test cases that must be executed for the compute method.
+  const numValidComputeTestCases: u32 = 100;
+
+  // how many total test cases (valid + rejected) that may be attempted.
+  //   0 means all inputs must satisfy the precondition (if present),
+  //   5 means at most 5 rejected inputs are allowed per valid test case
+  const computeRejectRatio: u32 = 5;
+
+  const verbosity: u32 = 2;
+
+  testInitializeCB_macro! {
+    prop_testInitializeCB_macro, // test name
+    config: ProptestConfig { // proptest configuration, built by overriding fields from default config
+      cases: numValidComputeTestCases,
+      max_global_rejects: numValidComputeTestCases * computeRejectRatio,
+      verbose: verbosity,
+      ..ProptestConfig::default()
+    }
+  }
+
+  testComputeCB_macro! {
+    prop_testComputeCB_macro, // test name
+    config: ProptestConfig { // proptest configuration, built by overriding fields from default config
+      cases: numValidComputeTestCases,
+      max_global_rejects: numValidComputeTestCases * computeRejectRatio,
+      verbose: verbosity,
+      ..ProptestConfig::default()
+    },
+    // strategies for generating each component input
+    api_request_log: generators::option_strategy_default(generators::Common_Request_Impl_strategy_default()),
+    api_response_log_in: generators::option_strategy_default(generators::Common_ResponseLog_Impl_strategy_default())
+  }
+
+  testComputeCBwGSV_macro! {
+    prop_testComputeCBwGSV_macro, // test name
+    config: ProptestConfig { // proptest configuration, built by overriding fields from default config
+      cases: numValidComputeTestCases,
+      max_global_rejects: numValidComputeTestCases * computeRejectRatio,
+      verbose: verbosity,
+      ..ProptestConfig::default()
+    },
+    // strategies for generating each component input
+    In_is_valid: generators::bool_strategy_default(),
+    In_since_result: generators::bool_strategy_default(),
+    api_request_log: generators::option_strategy_default(generators::Common_Request_Impl_strategy_default()),
+    api_response_log_in: generators::option_strategy_default(generators::Common_ResponseLog_Impl_strategy_default())
+  }
+}
--- Models/Status/Status_v2/Hardened/hamr/microkit/crates/cpuSw_reportMonitor_reportMonitor/src/test/tests.rs
+++ Models/Status/Status_v2/Hardened/hamr/microkit/crates/cpuSw_reportMonitor_reportMonitor/src/test/tests.rs
@@ -21,3 +21,63 @@ mod tests {
     crate::cpuSw_reportMonitor_reportMonitor_timeTriggered();
   }
 }
+
+mod GUMBOX_tests {
+  use serial_test::serial;
+  use proptest::prelude::*;
+
+  use crate::test::util::*;
+  use crate::testInitializeCB_macro;
+  use crate::testComputeCB_macro;
+    use crate::testComputeCBwGSV_macro;
+
+  // number of valid (i.e., non-rejected) test cases that must be executed for the compute method.
+  const numValidComputeTestCases: u32 = 100;
+
+  // how many total test cases (valid + rejected) that may be attempted.
+  //   0 means all inputs must satisfy the precondition (if present),
+  //   5 means at most 5 rejected inputs are allowed per valid test case
+  const computeRejectRatio: u32 = 5;
+
+  const verbosity: u32 = 2;
+
+  testInitializeCB_macro! {
+    prop_testInitializeCB_macro, // test name
+    config: ProptestConfig { // proptest configuration, built by overriding fields from default config
+      cases: numValidComputeTestCases,
+      max_global_rejects: numValidComputeTestCases * computeRejectRatio,
+      verbose: verbosity,
+      ..ProptestConfig::default()
+    }
+  }
+
+  testComputeCB_macro! {
+    prop_testComputeCB_macro, // test name
+    config: ProptestConfig { // proptest configuration, built by overriding fields from default config
+      cases: numValidComputeTestCases,
+      max_global_rejects: numValidComputeTestCases * computeRejectRatio,
+      verbose: verbosity,
+      ..ProptestConfig::default()
+    },
+    // strategies for generating each component input
+    api_analysis_report_in: generators::option_strategy_default(generators::Common_AnalysisReport_Impl_strategy_default()),
+    api_analysis_request: generators::option_strategy_default(generators::Common_AnalysisRequest_Impl_strategy_default())
+  }
+
+  testComputeCBwGSV_macro! {
+    prop_testComputeCBwGSV_macro, // test name
+    config: ProptestConfig { // proptest configuration, built by overriding fields from default config
+      cases: numValidComputeTestCases,
+      max_global_rejects: numValidComputeTestCases * computeRejectRatio,
+      verbose: verbosity,
+      ..ProptestConfig::default()
+    },
+    // strategies for generating each component input
+    In_counter: generators::u32_strategy_default(),
+    In_is_invalid2: generators::bool_strategy_default(),
+    In_is_valid1: generators::bool_strategy_default(),
+    In_since_result: generators::bool_strategy_default(),
+    api_analysis_report_in: generators::option_strategy_default(generators::Common_AnalysisReport_Impl_strategy_default()),
+    api_analysis_request: generators::option_strategy_default(generators::Common_AnalysisRequest_Impl_strategy_default())
+  }
+}
