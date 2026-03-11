4e4c4747c0cc227a934b53634051ff0bc906a09d
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-03-11 15:46:09 -0400

    Add UART debug into wifiDriver

--- Models/Status/Status_v2/Hardened/hamr/microkit/crates/cpuSw_wifiDriver_wifiDriver/src/component/cpuSw_wifiDriver_wifiDriver_app.rs
+++ Models/Status/Status_v2/Hardened/hamr/microkit/crates/cpuSw_wifiDriver_wifiDriver/src/component/cpuSw_wifiDriver_wifiDriver_app.rs
@@ -3,21 +3,36 @@
 use data::*;
 use crate::bridge::cpuSw_wifiDriver_wifiDriver_api::*;
 use vstd::prelude::*;
+use sel4_microkit_base::{memory_region_symbol};
+
+// UART specific crates:
+use sel4_pl011_driver::Driver as UartDriver;
+use embedded_hal_nb::serial::Write;
 
 verus! {
+
+  fn uart_put_str(driver: &mut UartDriver, str: &str) {
+     for c in str.chars(){
+          let _ = driver.write(c as u8);
+     }
+   }
 
   pub struct cpuSw_wifiDriver_wifiDriver {
     // BEGIN MARKER STATE VARS
     pub alert_cmd: bool,
+    uart_debug: UartDriver,
     // END MARKER STATE VARS
   }
 
   impl cpuSw_wifiDriver_wifiDriver {
     pub fn new() -> Self
     {
+     let uart_driver = 
+        unsafe { UartDriver::new(memory_region_symbol!(uart_base_vaddr: *mut ()).as_ptr()) };
       Self {
         // BEGIN MARKER STATE VAR INIT
         alert_cmd: false,
+        uart_debug: uart_driver,
         // END MARKER STATE VAR INIT
       }
     }
@@ -31,7 +46,9 @@ verus! {
         self.alert_cmd == false,
         // END MARKER INITIALIZATION ENSURES
     {
+      self.uart_debug.init();
       log_info("initialize entrypoint invoked");
+      uart_put_str(&mut self.uart_debug, "Hello from UART land!\n");
     }
 
     pub fn timeTriggered<API: cpuSw_wifiDriver_wifiDriver_Full_Api> (
@@ -63,6 +80,7 @@ verus! {
         // END MARKER TIME TRIGGERED ENSURES
     {
       log_info("compute entrypoint invoked");
+      uart_put_str(&mut self.uart_debug, "Hello from UART land again!\n");
     }
 
     pub fn notify(
