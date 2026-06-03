115bbadb389026464c3450716f7c5af6b30011dd
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-06-03 11:40:31 -0400

    Adjust rootfs location slightly for qemu

--- Models/Status/Status_v2/Hardened/hamr/microkit_mcs/components/cpuSw_wifiDriver_wifiDriverVM_wifiDriver/include/cpuSw_wifiDriver_wifiDriverVM_wifiDriver_user.h
+++ Models/Status/Status_v2/Hardened/hamr/microkit_mcs/components/cpuSw_wifiDriver_wifiDriverVM_wifiDriver/include/cpuSw_wifiDriver_wifiDriverVM_wifiDriver_user.h
@@ -10,8 +10,8 @@
 // This file will not be overwritten if HAMR codegen is rerun
 
 #if defined(BOARD_qemu_virt_aarch64)
-#define GUEST_DTB_VADDR           0x4f000000
-#define GUEST_INIT_RAM_DISK_VADDR 0x4d700000
+#define GUEST_DTB_VADDR           0x47f00000
+#define GUEST_INIT_RAM_DISK_VADDR 0x47000000
 #elif defined(BOARD_rpi4b_4gb)
 #define GUEST_DTB_VADDR           0x2f000000
 #define GUEST_INIT_RAM_DISK_VADDR 0x2d700000
